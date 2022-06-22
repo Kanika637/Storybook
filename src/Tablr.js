b35286a829859bbba338f5369a697ee762b0a919
Kanika Gola <kanikagola26@gmail.com>
2022-06-22 20:02:10 +0530

    Forming the component

---
+++ src/Tablr.js
@@ -0,0 +1,6 @@
+export const Tablr=()=>{
+    return(
+        <h1>Tablr</h1>
+        
+    )
+}
---
+++ src/stories/Tablr.stories.js
@@ -0,0 +1,10 @@
+import { Tablr } from "../Tablr";
+
+export default{
+    title: 'Tablr - A Customizable React Table Component',
+    componenet: Tablr,
+};
+
+const Template= args => <Tablr/>;
+
+export const Default = Template.bind({});
